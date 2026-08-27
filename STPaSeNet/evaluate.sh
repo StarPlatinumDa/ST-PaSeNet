e60a01591e977f17b798e80111f2e9a0267dc753
star platinum <41507419+StarPlatinumDa@users.noreply.github.com>
2026-08-28 00:17:37 +0800

    Delete STPaSeNet directory

--- STPaSeNet/evaluate.sh
+++
@@ -1,3 +0,0 @@
-python mainStage80.py --weights pretrained weights/ntu 60/joint/runs-128-80128.pt --phase test --save-score True --config config/nturgbd-cross-subject/joint.yaml --device 0 --start-epoch 128
-
-python mainStage80.py --weights pretrained weights/ntu 120/joint/runs-124-122016.pt --phase test --save-score True --config config/nturgbd120-cross-subject/joint.yaml --device 0 --start-epoch 124
